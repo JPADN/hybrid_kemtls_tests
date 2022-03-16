f167aaa194870db65a06f52e3dab93df35658be9
JPADN <jpadnalemao@gmail.com>
2022-03-16 12:03:17 -0300

    Minor fix in install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ cd ${WORKING_DIR}
 # Setting LD_LIBRARY_PATH to point to the path to liboqs' library directory, e.g.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-echo "'/usr/local/lib' was appended temporarily to LD_LIBRARY_PATH, to make it permanent append the following in your `~/.profile `:
+echo "'/usr/local/lib' was appended temporarily to LD_LIBRARY_PATH, to make it permanent append the following in your ~/.profile:
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/lib
 "
 
@@ -21,7 +21,7 @@ git clone --branch 0.7.1 https://github.com/open-quantum-safe/liboqs-go
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${WORKING_DIR}/liboqs-go/.config
 
-echo "'${WORKING_DIR}/liboqs-go/.config' was appended temporarily to PKG_CONFIG_PATH, to make it permanent append the following in your `~/.profile `:
+echo "'${WORKING_DIR}/liboqs-go/.config' was appended temporarily to PKG_CONFIG_PATH, to make it permanent append the following in your ~/.profile:
 export PKG_CONFIG_PATH=\$PKG_CONFIG_PATH:${WORKING_DIR}/liboqs-go/.config
 "
 
@@ -30,6 +30,6 @@ cd ${GO_KEMTLS_ROOT}/src
 
 export PATH=${GO_KEMTLS_ROOT}/bin:$PATH:
 
-echo "'${GO_KEMTLS_ROOT}/bin' was appended temporarily to PATH, to make it permanent append the following in your `~/.profile `:
+echo "'${GO_KEMTLS_ROOT}/bin' was appended temporarily to PATH, to make it permanent append the following in your ~/.profile:
 export PATH=\${GO_KEMTLS_ROOT}/bin:$PATH:
 "
